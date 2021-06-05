a020129c2b081935f0d8f93a94ca3b67eada70b0
Enrico Gallus <enrico.gallus@gmail.com>
2021-06-05 15:42:09 +0900

    adding code files

---
+++ FileWatcher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        private static string pathToFileToObserve;
+        private static string targetFileName;
+        
+        static void Main(string[] args)
+        {
+            pathToFileToObserve = Path.Combine(args[0], args[1]);
+            targetFileName = Path.Combine(args[0], "targetFile.txt");
+            var watch = new FileSystemWatcher
+            {
+                Path = args[0],
+                Filter = args[1],
+                NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite,
+                EnableRaisingEvents = true
+            };
+            watch.Changed += OnChanged;
+            Console.ReadLine();
+        }
+        
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                string lastLine = File.ReadLines(pathToFileToObserve).Last();
+                using (StreamWriter file = new StreamWriter(targetFileName))
+                {
+                    file.Write(lastLine);
+                }
+            }
+        }
+    }
+}
